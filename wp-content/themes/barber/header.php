3039b3010727699c9ed28d67dcc4d5584d998b61
justintsmith <justin@justintsmith.com>
2015-06-12 16:16:13 -0500

    start styling the header

--- wp-content/themes/barber/header.php
+++ wp-content/themes/barber/header.php
@@ -46,21 +46,19 @@
 
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-				<div id="inner-header" class="wrap cf">
+					<!-- LOGO -->
+					<a href="<?php echo home_url(); ?>" rel="nofollow" id="logo">
+						<img src="<?php bloginfo('stylesheet_directory'); ?>/library/images/joes-logo.svg" alt="Joe's Barbershop Chicago Logo">
+						<h1 class="logo-text" itemscope itemtype="http://schema.org/Organization"><?php bloginfo('name'); ?></h1>
+					</a>
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
-
-
+					<!-- MAIN NAV -->
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<?php wp_nav_menu(array(
     					         'container' => false,                           // remove nav container
     					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
     					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
+    					         'menu_class' => 'nav top-nav nav-collapse',               // adding custom nav class
     					         'theme_location' => 'main-nav',                 // where it's located in the theme
     					         'before' => '',                                 // before the menu
         			               'after' => '',                                  // after the menu
@@ -71,7 +69,5 @@
 						)); ?>
 
 					</nav>
-
-				</div>
 
 			</header>
