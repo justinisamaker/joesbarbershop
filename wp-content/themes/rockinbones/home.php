c1d29642b3a2001f98d7df01fcc355e840b78ef4
Justin Smith <justin@isamaker.com>
2015-08-25 13:54:05 -0500

    Continue building out the home page

--- wp-content/themes/rockinbones/home.php
+++ wp-content/themes/rockinbones/home.php
@@ -18,12 +18,30 @@
       </section>
 
       <section id="shop-info" class="home-mid-section">
-        <ul id="shop-prices">
-          <li><span>Haircut:</span> $25</li>
-          <li><span>Shave:</span> $30</li>
-          <li><span>Sunday Services:</span> $30</li>
-          <li><span>Any service after posted shop hours:</span> $50</li>
-        </ul>
+        <div id="prices" class="info-section">
+          <h3>Prices</h3>
+          <div class="underline"></div>
+          <ul id="shop-prices">
+            <li><span>Haircut:</span> $25</li>
+            <li><span>Shave:</span> $30</li>
+            <li><span>Sunday Services:</span> $30</li>
+            <li><span>Any service after posted shop hours:</span> $50</li>
+          </ul>
+        </div>
+
+        <div id="hours" class="info-section">
+          <h3>Hours</h3>
+          <div class="underline"></div>
+          <ul id="shop-hours">
+            <li><span>Monday:</span> Shop Closed</li>
+            <li><span>Tuesday:</span> 9:00 AM - 5:30 PM</li>
+            <li><span>Wednesday:</span> 9:00 AM - 5:30 PM</li>
+            <li><span>Thursday:</span> 9:00 AM - 7:30 PM</li>
+            <li><span>Friday:</span> 9:00 AM - 5:30 PM</li>
+            <li><span>Saturday:</span> 9:00 AM - 3:00 PM</li>
+            <li><span>Sunday:</span> 10:00 AM - 2:00 PM</li>
+          </ul>
+        </div>
 
         <!-- Contact
         773-252-3980
@@ -45,7 +63,7 @@
 
       <section id="events">
         <div id="events-container">
-          <?php 
+          <?php
             $args = array(
               'post_type' => 'event'
             );
@@ -55,18 +73,18 @@
           <?php
             if( $query->have_posts() ): while( $query->have_posts() ): $query->the_post();
           ?>
-            <div id="event-info">
+            <div class="event-info">
               <h3><?php the_title(); ?></h3>
               <?php if( get_field('event_date') ): ?>
-                <h5>
+                <h5 class="event-date">
                   <?php the_field('event_date'); ?>
                 </h5>
               <?php endif; ?>
 
               <?php if( get_field('event_time') ): ?>
-                <h6>
+                <h5 class="event-time">
                   <?php the_field('event_time'); ?>
-                </h6>
+                </h5>
               <?php endif; ?>
 
               <?php if( get_field('event_description') ): ?>
@@ -81,20 +99,62 @@
                 </a>
               <?php endif; ?>
             </div> <!-- END EVENT INFO -->
-            <div id="event-photo">
-              <?php 
+            <div class="event-photo">
+              <?php
               if ( has_post_thumbnail() ) {
                 the_post_thumbnail();
-              } 
+              }
               ?>
             </div>
           <?php endwhile; endif; wp_reset_postdata(); ?>
         </div>
       </section>
 
-      <!-- 
+      <section id="location">
+        <div id="location-container">
+          <a href="https://www.google.com/maps/place/Joe's+Barbershop+Chicago/@41.92464,-87.694206,17z/data=!3m1!4b1!4m2!3m1!1s0x880fd29cebe3d04f:0x189bb8818adecec3" target="_blank">2641 W Fullerton, Chicago, IL 60647</a>
+
+          <a href="mailto:shopinfo@joesbarbershopchicago.com" target="_blank">shopinfo@joesbarbershopchicago.com</a>
+
+          <a href="tel:7732523980" target="_blank">773-252-3980</a>
+        </div>
+      </section>
+
+      <section id="home-social">
+        <div id="home-social-container">
+          <a href="http://www.facebook.com/pages/Joes-Barber-Shop-Chicago/178010742775" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/facebook.png" width="50" height="50" border="0" alt="Joe's Barbershop Chicago on Facebook" />
+          </a>
+
+          <a href="http://instagram.com/joesbarbershopchicago1" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/instagram.png" width="50" height="50" border="0" alt="Joe's Barbershop Chicago on Instagram" />
+          </a>
+
+          <a href="http://twitter.com/JoesBarberShopp" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/twitter.png" width="50" height="50" border="0" alt="Joe's Barbershop Chicago on Twitter" />
+          </a>
+
+          <a href="http://www.yelp.com/biz/joes-barbershop-chicago-2" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/yelp.png" width="50" height="50" border="0" alt="Joe's Barbershop Chicago on Yelp" />
+          </a>
+
+          <a href="http://joesbarbershopchicago.tumblr.com/" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/tumblr.png" width="50" height="50" border="0" alt="Joe's Barbershop Chicago on Tumblr" />
+          </a>
+
+          <a href="https://foursquare.com/v/joes-barbershop-chicago/4b8d2ca3f964a5208feb32e3?ref=atw" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/foursquare.png" width="50" height="50" border="0" alt="Joe's Barbershop Chicago on Foursquare" />
+          </a>
+
+          <a href="http://www.youtube.com/user/JoesBarbershopChi1" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/youtube.png" width="50" height="50" border="0" alt="Joe's Barbershop Chicago on YouTube" />
+          </a>
+        </div>
+      </section> <!-- END SOCIAL -->
+
+      <!--
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        
+
         <h2><?php the_title(); ?></h2>
         <p><?php the_content(); ?></p>
 
