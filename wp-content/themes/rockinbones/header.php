ccc9b7fa234f68e6f299c4f4bbd2c0590dc07685
justintsmith <justin@justintsmith.com>
2015-08-29 20:06:12 -0500

    Add meta viewport to header

--- wp-content/themes/rockinbones/header.php
+++ wp-content/themes/rockinbones/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <title><?php wp_title(); ?></title>
-
+    <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
